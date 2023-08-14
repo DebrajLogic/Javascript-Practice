220adbbcb9b63bdf9559443f399e2760d4b27d2b
Debraj <81313461+DebrajLogic@users.noreply.github.com>
2023-08-14 07:42:21 +0000

    Basic Calculation using two operands working

---
+++ Basic_DOM_Projects/Calculator/index.js
@@ -0,0 +1,136 @@
+const input = document.querySelectorAll('.input-btn');
+
+const output = document.querySelector('#calculate');
+
+let cursor = document.querySelector('#display');
+
+const clearValue = document.querySelector('#CE');
+
+array = [];
+
+let operator = '';
+
+input.forEach((button)=>{
+    clearFlag = true
+    button.addEventListener('click', ()=>{
+        let value = button.innerHTML;
+        // console.log(value);
+         if(clearFlag){
+            clear();
+            clearFlag = false;
+         }
+
+        setOperator(value);
+        
+        getValue(value);
+        
+    })
+})
+
+clearValue.addEventListener('click',()=>{
+    clear();
+})
+
+
+output.addEventListener('click', (e)=>{
+    result();  
+})
+
+function setOperator(value){
+    switch(value){
+        case '+': operator = '+';
+        break;
+        case '-': operator = '-';
+        break;
+        case '*': operator = '*';
+        break;
+        case '/': operator = '/';
+        break;
+    }
+   
+}
+
+function extract(string){
+
+    let answer;
+    let operandA;
+    let operandB;
+
+    if(operator === '+'){
+        array = string.split('+');
+        console.log(array);
+        answer = array.reduce((acc, e)=>(acc += parseInt(e)), 0);    
+    }
+    if(operator === '-'){
+        array = string.split('-');
+        console.log(array);
+        operandA = parseFloat(array[0]);
+        operandB = parseFloat(array[1]);
+        answer = subtract(operandA, operandB); 
+    }
+    if(operator === '*'){
+        array = string.split('*');
+        console.log(array);
+        operandA = parseFloat(array[0]);
+        operandB = parseFloat(array[1]);
+        answer = multiply(operandA, operandB);
+    }
+    if(operator === '/'){
+        array = string.split('/');
+        console.log(array);
+        operandA = parseFloat(array[0]);
+        operandB = parseFloat(array[1]);
+        if(operandB == 0){
+            answer = 'undefined'
+            return;
+        }
+            
+        answer = divide(operandA, operandB);    
+    }
+    
+    console.log(operandA);
+    console.log(operandB);
+    console.log(answer);
+    
+    return answer;
+}
+
+
+function result(){
+    string = cursor.innerHTML;
+    let answer = extract(string);  
+    answer = `${answer}`;
+    console.log(typeof answer);
+    showResult(answer);
+}
+
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    let quotient = a/b;
+    console.log('quotient = ', quotient);
+    
+    let remainder = (a % b)/100;
+    console.log('remainder = ', remainder);
+
+    return (quotient) + remainder;
+}
+
+function clear(){
+    cursor.innerHTML = '';
+}
+
+function getValue(value){
+    cursor.innerHTML += value;
+}
+
+function showResult(answer){
+    cursor.innerHTML = answer;
+}
